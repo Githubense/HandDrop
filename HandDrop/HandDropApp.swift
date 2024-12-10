de2e2e3d195c18c511ff5aa82b0c1e5bdd1c4a35
Githubense <angelopf@outlook.com>
2024-12-10 13:41:18 +0100

    Initial Commit

---
+++ HandDrop/HandDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandDropApp.swift
+//  HandDrop
+//
+//  Created by Angel Adrian Pimienta Flores on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HandDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
